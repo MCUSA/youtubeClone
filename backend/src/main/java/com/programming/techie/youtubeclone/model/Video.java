cab6db58826b77b666fdb9eac5b39c4eb8dd6f45
mayurchinchalkar@gmail.com <Jyoti@1152>
2022-10-20 13:34:18 +0530

    Initial push

---
+++ backend/src/main/java/com/programming/techie/youtubeclone/model/Video.java
@@ -0,0 +1,29 @@
+package com.programming.techie.youtubeclone.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+import java.util.Set;
+
+@Document(value="Videos")
+public class Video {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String userId;
+    private Integer likes;
+    private Integer disLikes;
+    private Set<String> tags;
+    private String videoUrl;
+    private VideoStatus videoStatus;
+    private Integer viewCount;
+    private String thumbnailUrl;
+    private List<Comment> commentList;
+
+}
