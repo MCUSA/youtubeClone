00cfc0725481fab22de9703444be3963a5fa15a8
mayurchinchalkar@gmail.com <Jyoti@1152>
2022-10-20 13:57:31 +0530

    updated Video.java and backend/pom file with the dependency of document

--- backend/src/main/java/com/programming/techie/youtubeclone/model/Video.java
+++ backend/src/main/java/com/programming/techie/youtubeclone/model/Video.java
@@ -2,9 +2,6 @@ package com.programming.techie.youtubeclone.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 import java.util.Set;
